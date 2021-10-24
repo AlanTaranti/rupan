9a52150e0bfef4f3096b9a768962a4476c261553
Alan Taranti <alan.taranti@gmail.com>
2021-10-24 18:51:28 -0400

    simplificado codigoo

--- src/security_group_service.py
+++ src/security_group_service.py
@@ -1,20 +1,8 @@
-from typing import List, Dict
+from typing import List
 
-from .modules.base import BaseModule
 from .modules.ec2 import Ec2
 
 
 class SecurityGroupService:
-    @property
-    def modules(self) -> Dict[str, BaseModule]:
-        return {
-            "security-groups": Ec2,
-        }
-
     def get_security_groups(self) -> List:
-        security_groups = {}
-        for module_name, module_class in self.modules.items():
-            module_instance = module_class()
-            security_groups[module_name] = module_instance.get_segurity_groups()
-
-        return security_groups
+        return Ec2().get_segurity_groups()
