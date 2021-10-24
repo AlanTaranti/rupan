4100e8184ad7996a2e944c3ca1c8fe71287bb696
Alan Taranti <alan.taranti@gmail.com>
2021-10-24 16:53:08 -0400

    adicionado parametros opcionais em ec2 sg

--- src/modules/models/ec2_security_group.py
+++ src/modules/models/ec2_security_group.py
@@ -1,10 +1,14 @@
 from dataclasses import dataclass
+from typing import List, Optional
+
+from .tag import Tag
 
 
 @dataclass
 class EC2SecurityGroup:
 
-    Status: str
+    Status: Optional[str]
     EC2SecurityGroupName: str
     EC2SecurityGroupId: str
     EC2SecurityGroupOwnerId: str
+    Tags: Optional[List[Tag]]
