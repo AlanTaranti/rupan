7a16672e94e422bca4b1819c213aa929b1bc2f65
Alan Taranti <alan.taranti@gmail.com>
2021-10-24 18:29:02 -0400

    adicionado servicos nao utilizados

--- src/security_group_service.py
+++ src/security_group_service.py
@@ -2,9 +2,6 @@ from typing import List, Dict
 
 from .modules.base import BaseModule
 from .modules.ec2 import Ec2
-from .modules.rds import Rds
-from .modules.redshift import Redshift
-from .modules.elasticache import Elasticache
 
 
 class SecurityGroupService:
@@ -12,9 +9,6 @@ class SecurityGroupService:
     def modules(self) -> Dict[str, BaseModule]:
         return {
             "ec2": Ec2,
-            "rds": Rds,
-            "redshift": Redshift,
-            "elasticache": Elasticache,
         }
 
     def get_security_groups(self) -> List:
