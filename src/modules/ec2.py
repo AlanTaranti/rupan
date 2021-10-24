f2b8e3d60c0aeea86f652d0bd3a12b53ce9c70f3
Alan Taranti <alan.taranti@gmail.com>
2021-10-24 14:38:40 -0400

    adicionado modulo ec2

---
+++ src/modules/ec2.py
@@ -0,0 +1,12 @@
+from typing import List
+import boto3
+from .base import BaseModule
+
+
+class Ec2(BaseModule):
+    @property
+    def client(self):
+        return boto3.client("ec2")
+
+    def get_segurity_groups(self) -> List:
+        return self.client.describe_security_groups()['SecurityGroups']
