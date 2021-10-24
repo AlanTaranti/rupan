e3e13c12f88402bd57c4b37aa69c430e7f5853a9
Alan Taranti <alan.taranti@gmail.com>
2021-10-24 16:19:07 -0400

    definido tipo de retorno de ec2

--- src/modules/ec2.py
+++ src/modules/ec2.py
@@ -1,5 +1,7 @@
 from typing import List
 import boto3
+
+from .models.security_group import SecurityGroup
 from .base import BaseModule
 
 
@@ -8,5 +10,5 @@ class Ec2(BaseModule):
     def client(self):
         return boto3.client("ec2")
 
-    def get_segurity_groups(self) -> List:
+    def get_segurity_groups(self) -> List[SecurityGroup]:
         return self.client.describe_security_groups()["SecurityGroups"]
