975a5abb3bf8965ce8440be0b1e761d332aa599f
Alan Taranti <alan.taranti@gmail.com>
2021-10-25 14:35:47 -0400

    alterado script para gera uma planilha apenas

--- aws-sg-extractor.py
+++ aws-sg-extractor.py
@@ -1,4 +1,5 @@
 import os
+from collections import OrderedDict
 
 import fire
 import pandas as pd
@@ -6,27 +7,27 @@ import pandas as pd
 from src.security_group_service import SecurityGroupService
 
 
-def ip_permissions_formatter(ip_permissions: pd.DataFrame) -> pd.DataFrame:
+def ip_permissions_formatter(ip_permissions: pd.DataFrame, prefix) -> pd.DataFrame:
     ip_permissions = ip_permissions.rename(
         columns={
-            "FromPort": "from_port",
-            "IpProtocol": "ip_protocol",
-            "IpRanges": "ipv4_ranges",
-            "Ipv6Ranges": "ipv6_ranges",
-            "PrefixListIds": "prefix_list_ids",
-            "ToPort": "to_port",
-            "UserIdGroupPairs": "user_id_group_pairs",
+            "FromPort": "{}_from_port".format(prefix),
+            "IpProtocol": "{}_ip_protocol".format(prefix),
+            "IpRanges": "{}_ipv4_ranges".format(prefix),
+            "Ipv6Ranges": "{}_ipv6_ranges".format(prefix),
+            "PrefixListIds": "{}_prefix_list_ids".format(prefix),
+            "ToPort": "{}_to_port".format(prefix),
+            "UserIdGroupPairs": "{}_user_id_group_pairs".format(prefix),
         }
     )
 
     columns = [
-        "ip_protocol",
-        "from_port",
-        "to_port",
-        "ipv4_ranges",
-        "ipv6_ranges",
-        "prefix_list_ids",
-        "user_id_group_pairs",
+        "{}_ip_protocol".format(prefix),
+        "{}_to_port".format(prefix),
+        "{}_to_port".format(prefix),
+        "{}_ipv4_ranges".format(prefix),
+        "{}_ipv6_ranges".format(prefix),
+        "{}_prefix_list_ids".format(prefix),
+        "{}_user_id_group_pairs".format(prefix),
     ]
 
     ip_permissions = ip_permissions.reindex(columns=columns)
@@ -44,41 +45,47 @@ def aws_sg_extractor(profile: str = "default"):
     security_groups = service.get_security_groups()
 
     # Criar diretorio de saida de arquivos
-    base_dir = os.path.join("output", profile)
+    base_dir = "output"
     os.makedirs(base_dir, exist_ok=True)
+    filepath = os.path.join(base_dir, "{}.csv".format(profile))
 
+    dataframes = []
     # tratar os security groups
     for security_group in security_groups:
-        name = security_group["GroupName"]
-        filename = "{}.xlsx".format(name)
-        filepath = os.path.join(base_dir, filename)
 
         # Metadados
-        metadata = {
-            "group_name": security_group["GroupName"],
-            "owner_id": security_group["OwnerId"],
-            "group_id": security_group["GroupId"],
-            "vpc_id": security_group["VpcId"],
-            "description": security_group["Description"],
-        }
-        metadata = pd.DataFrame([metadata])
+        metadata = OrderedDict(
+            {
+                "group_name": security_group["GroupName"],
+                "owner_id": security_group["OwnerId"],
+                "group_id": security_group["GroupId"],
+                "vpc_id": security_group["VpcId"],
+                "description": security_group["Description"],
+            }
+        )
+        metadata = OrderedDict(reversed(list(metadata.items())))
 
         # Ip Permissions
         ip_permissions_inbound = pd.DataFrame(security_group["IpPermissions"])
-        ip_permissions_inbound = ip_permissions_formatter(ip_permissions_inbound)
+        ip_permissions_inbound = ip_permissions_formatter(
+            ip_permissions_inbound, "ip_permissions_inbound"
+        )
 
         # Ip Permissions Egress
         ip_permissions_outbound = pd.DataFrame(security_group["IpPermissionsEgress"])
-        ip_permissions_outbound = ip_permissions_formatter(ip_permissions_outbound)
+        ip_permissions_outbound = ip_permissions_formatter(
+            ip_permissions_outbound, "ip_permissions_outbound"
+        )
 
-        with pd.ExcelWriter(filepath, mode="w") as writer:
-            metadata.to_excel(writer, "metadata", index=False)
-            ip_permissions_inbound.to_excel(
-                writer, "ip_permissions_inbound", index=False
-            )
-            ip_permissions_outbound.to_excel(
-                writer, "ip_permissions_outbound", index=False
-            )
+        dataframe = pd.concat([ip_permissions_inbound, ip_permissions_outbound], axis=1)
+
+        for key, value in metadata.items():
+            dataframe.insert(0, key, value)
+
+        dataframes.append(dataframe)
+
+    dataframes = pd.concat(dataframes)
+    dataframes.to_csv(filepath, index=False)
 
 
 if __name__ == "__main__":
