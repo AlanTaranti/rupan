ac9f090ba165eecdddb2780c3b634ef606fa22aa
Alan Taranti <alan.taranti@gmail.com>
2021-11-03 10:22:52 -0400

    adicionado regiao

--- src/repository/base_repository.py
+++ src/repository/base_repository.py
@@ -5,12 +5,15 @@ from botocore.exceptions import ProfileNotFound
 
 
 class BaseRepository:
-    def __init__(self, profile="default"):
+    def __init__(self, profile="default", region_name: str = None):
         self.profile = profile
+        self.region_name = region_name
 
     @property
     def session(self):
         try:
-            return boto3.Session(profile_name=self.profile)
+            return boto3.Session(
+                profile_name=self.profile, region_name=self.region_name
+            )
         except ProfileNotFound:
             exit("Perfil {}, não encontrado!".format(self.profile))
