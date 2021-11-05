886a99dd86f4b14789cd29f45550b9dd7af29a48
Alan Taranti <alan.taranti@gmail.com>
2021-11-04 23:51:33 -0400

    removido import e alterado nome de arquivo

--- aws-sg-extractor.py
+++ aws-sg-extractor.py
@@ -2,23 +2,25 @@ import os
 
 import fire
 
+from src.aws_account_service import AwsAccountService
 from src.security_group_service import SecurityGroupService
 
 
-def aws_sg_extractor(profile: str = "default"):
+def aws_sg_extractor():
     """
     Um simples extrator de Security Groups da AWS
     """
 
     # Obter os security groups
-    service = SecurityGroupService(profile=profile)
+    security_group_service = SecurityGroupService()
+    aws_service = AwsAccountService()
 
     # Criar diretorio de saida de arquivos
     base_dir = "output"
     os.makedirs(base_dir, exist_ok=True)
-    filepath = os.path.join(base_dir, "{}.csv".format(profile))
+    filepath = os.path.join(base_dir, "{}.csv".format(aws_service.get_account_id()))
 
-    dataframes = service.to_pandas()
+    dataframes = security_group_service.to_pandas()
     dataframes.to_csv(filepath, index=False)
 
 
