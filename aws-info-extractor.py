8a2f02676ad7ce2673a3cca6c57f815fe8ae2392
Alan Taranti <alan.taranti@gmail.com>
2021-11-08 10:20:27 -0400

    adicionado salvamento em csv

--- aws-info-extractor.py
+++ aws-info-extractor.py
@@ -4,6 +4,19 @@ import fire
 
 from src.aws_account_service import AwsAccountService
 from src.security_group_service import SecurityGroupService
+from src.access_key_service import AccessKeyService
+
+
+def get_filepath(module_name: str):
+    base_dir = "output"
+    os.makedirs(base_dir, exist_ok=True)
+    account_id = get_account_id()
+    return os.path.join(base_dir, "{}_{}.csv".format(account_id, module_name))
+
+
+def get_account_id():
+    aws_service = AwsAccountService()
+    return aws_service.get_account_id()
 
 
 def aws_sg_extractor():
@@ -13,16 +26,35 @@ def aws_sg_extractor():
 
     # Obter os security groups
     security_group_service = SecurityGroupService()
-    aws_service = AwsAccountService()
 
-    # Criar diretorio de saida de arquivos
-    base_dir = "output"
-    os.makedirs(base_dir, exist_ok=True)
-    filepath = os.path.join(base_dir, "{}.csv".format(aws_service.get_account_id()))
+    # Obter Filepath
+    filepath = get_filepath("security_group")
 
+    # Obter dados
     dataframes = security_group_service.to_pandas()
+
+    # Salvar Dados
     dataframes.to_csv(filepath, index=False)
 
 
+def access_key_extractor():
+    """
+    Um simples extrator de Access Key da AWS
+    """
+    service = AccessKeyService()
+    access_keys = service.get_access_keys()
+
+    # Obter Filepath
+    filepath = get_filepath("access_key")
+
+    # Salvar Dados
+    access_keys.to_csv(filepath, index=False)
+
+
 if __name__ == "__main__":
-    fire.Fire({"security-group": aws_sg_extractor})
+    fire.Fire(
+        {
+            "security-group": aws_sg_extractor,
+            "access-keys": access_key_extractor,
+        }
+    )
