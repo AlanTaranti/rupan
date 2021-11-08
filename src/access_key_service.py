f9d8fb345c9ee2e76fbc45505b6b476ad435066b
Alan Taranti <alan.taranti@gmail.com>
2021-11-08 10:34:35 -0400

    adicionado formatacao de last_used

--- src/access_key_service.py
+++ src/access_key_service.py
@@ -13,11 +13,14 @@ class AccessKeyService:
                 "AccessKeyId": "access_key",
                 "Status": "status",
                 "CreateDate": "create_date",
+                "LastUsedDate": "last_used_date",
+                "ServiceName": "last_used_service_name",
+                "Region": "last_used_region",
             }
         )
         return dataframe
 
-    def get_access_keys(self, format="pandas"):
+    def get_access_keys(self):
         repository = IamRepository()
         sts_repository = StsRepository()
 
@@ -26,17 +29,11 @@ class AccessKeyService:
         except ClientError as error:
             exit(error)
 
-        if format == "list":
-            return access_keys
-
-        try:
-            access_keys = repository.list_access_keys()
-        except ClientError as error:
-            exit(error)
-
         access_keys = pd.DataFrame(access_keys)
+
         account_id = sts_repository.get_account_id()
         access_keys.insert(0, "account_id", account_id)
+
         access_keys = self.__format_get_access_keys(access_keys)
 
         return access_keys
