fd21f57b3c36697f5854a509abd1de5513a90201
Alan Taranti <alan.taranti@gmail.com>
2021-11-08 10:20:54 -0400

    adicionado formatacao simples

--- src/access_key_service.py
+++ src/access_key_service.py
@@ -1,7 +1,25 @@
+import pandas as pd
+from botocore.exceptions import ClientError
+
 from .repository.iam_repository import IamRepository
 
 
 class AccessKeyService:
-    def get_access_keys(self):
+    def __format_get_access_keys(self, dataframe: pd.DataFrame):
+        return dataframe
+
+    def get_access_keys(self, format="pandas"):
         repository = IamRepository()
-        return repository.list_access_keys()
+
+        try:
+            access_keys = repository.list_access_keys()
+        except ClientError as error:
+            exit(error)
+
+        if format == "list":
+            return access_keys
+
+        access_keys = pd.DataFrame(access_keys)
+        access_keys = self.__format_get_access_keys(access_keys)
+
+        return access_keys
