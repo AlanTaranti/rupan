72f6ec88c02840083534adf6c162a86480c84432
Alan Taranti <alan.taranti@gmail.com>
2021-11-09 14:29:12 -0400

    adicionado logging service

---
+++ src/logging_service.py
@@ -0,0 +1,32 @@
+from datetime import datetime, timedelta
+
+import pandas as pd
+from botocore.exceptions import ClientError
+
+from .repository.sts_repository import StsRepository
+from .repository.s3_repository import S3Repository
+
+
+class LoggingService:
+    def __format_get_logging(self, dataframe: pd.DataFrame):
+        return dataframe
+
+    def get_logging(self):
+        repository = S3Repository()
+        sts_repository = StsRepository()
+
+        try:
+            buckets_logging = repository.list_buckets_logging()
+        except ClientError as error:
+            exit(error)
+
+        buckets_logging = pd.DataFrame(buckets_logging)
+        buckets_logging.insert(0, "service", "s3")
+
+        dataframe = pd.concat([buckets_logging])
+        account_id = sts_repository.get_account_id()
+        dataframe.insert(0, "account_id", account_id)
+
+        dataframe = self.__format_get_logging(dataframe)
+
+        return dataframe
