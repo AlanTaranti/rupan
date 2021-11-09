fd82ef10696d5aa1b93258dc89da478292761650
Alan Taranti <alan.taranti@gmail.com>
2021-11-09 15:05:17 -0400

    adicionado cloudfront em logging service

--- src/logging_service.py
+++ src/logging_service.py
@@ -5,6 +5,7 @@ from botocore.exceptions import ClientError
 
 from .repository.sts_repository import StsRepository
 from .repository.s3_repository import S3Repository
+from .repository.cloudfront_repository import CloudfrontRepository
 
 
 class LoggingService:
@@ -19,18 +20,27 @@ class LoggingService:
         return dataframe
 
     def get_logging(self):
-        repository = S3Repository()
+        s3_repository = S3Repository()
         sts_repository = StsRepository()
+        cloudfront_repository = CloudfrontRepository()
 
         try:
-            buckets_logging = repository.list_buckets_logging()
+            buckets_logging = s3_repository.list_buckets_logging()
+        except ClientError as error:
+            exit(error)
+
+        try:
+            cloudfront_logging = cloudfront_repository.list_distributions_logging()
         except ClientError as error:
             exit(error)
 
         buckets_logging = pd.DataFrame(buckets_logging)
         buckets_logging.insert(0, "service", "s3")
 
-        dataframe = pd.concat([buckets_logging])
+        cloudfront_logging = pd.DataFrame(cloudfront_logging)
+        cloudfront_logging.insert(0, "service", "cloudfront")
+
+        dataframe = pd.concat([buckets_logging, cloudfront_logging])
         account_id = sts_repository.get_account_id()
         dataframe.insert(0, "account_id", account_id)
 
